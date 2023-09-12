e6aabae4bcfad75533673f51e9a7ad2e574a2c5e
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-12 10:48:50 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -1,7 +1,11 @@
 local Translations = {
     info = {
-        ['hunters_called'] = "Iemand heeft je gezien en heeft de hunters gebeld!",
+        ['hunters_called'] = "Iemand heeft je gezien en heeft de hunters op je af gestuurd!",
         ['hunters_alive'] = "~r~%{count}~s~ hunters zoeken je nog.",
+        ['hunter_alive'] = "~r~%{count}~s~ hunter zoekt je nog.",
+        ['can_not_call_hunters'] = "Kan de hunters niet bellen, bel de politie!",
+        ["you_lose_the_hunters"] = "De hunters zijn je uit het oog verloren!",
+        ["you_lose_the_hunters"] = "Een hunter is je uit het oog verloren!",
     },
 }
 
