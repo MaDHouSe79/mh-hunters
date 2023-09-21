3d6d131ac6e08ed9bf61a87ce365dac3095a2177
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-21 17:30:01 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -102,6 +102,27 @@ local function HowManyHuntersAreStillAlive()
     return alive
 end
 
+local function SetPedOutfit(ped)
+    local data = Config.Outfit
+    local hearTexture = math.random(1,5)
+    local hearItem = math.random(1,2)
+    if data["hair"] ~= nil then SetPedComponentVariation(ped, 2, hearItem, hearTexture, 0) end
+    if data["beard"] ~= nil then SetPedComponentVariation(ped, 1, data["beard"].item, data["hair"].texture, 0) end
+    if data["pants"] ~= nil then SetPedComponentVariation(ped, 4, data["pants"].item, data["pants"].texture, 0) end
+    if data["arms"] ~= nil then SetPedComponentVariation(ped, 3, data["arms"].item, data["arms"].texture, 0) end
+    if data["t-shirt"] ~= nil then SetPedComponentVariation(ped, 8, data["t-shirt"].item, data["t-shirt"].texture, 0) end
+    if data["vest"] ~= nil then SetPedComponentVariation(ped, 9, data["vest"].item, data["vest"].texture, 0) end
+    if data["torso2"] ~= nil then SetPedComponentVariation(ped, 11, data["torso2"].item, data["torso2"].texture, 0) end
+    if data["shoes"] ~= nil then SetPedComponentVariation(ped, 6, data["shoes"].item, data["shoes"].texture, 0) end
+    if data["bag"] ~= nil then SetPedComponentVariation(ped, 5, data["bag"].item, data["bag"].texture, 0) end
+    if data["decals"] ~= nil then SetPedComponentVariation(ped, 10, data["decals"].item, data["decals"].texture, 0) end
+    if data["mask"] ~= nil then SetPedComponentVariation(ped, 1, data["mask"].item, data["mask"].texture, 0) end
+    if data["bag"] ~= nil then SetPedComponentVariation(ped, 5, data["bag"].item, data["bag"].texture, 0) end
+    if data["hat"] ~= nil and data["hat"].item ~= -1 and data["hat"].item ~= 0 then SetPedPropIndex(ped, 0, data["hat"].item, data["hat"].texture, true) end
+    if data["glass"] ~= nil and data["glass"].item ~= -1 and data["glass"].item ~= 0 then SetPedPropIndex(ped, 1, data["glass"].item, data["glass"].texture, true) end
+    if data["ear"] ~= nil and data["ear"].item ~= -1 and data["ear"].item ~= 0 then SetPedPropIndex(ped, 2, data["ear"].item, data["ear"].texture, true) end
+end
+
 local function loadModel(model)
     RequestModel(model)
     while not HasModelLoaded(model) do Wait(1) end
@@ -127,10 +148,12 @@ end
 
 local function createPed(coords, vehicle, seat)
     local model = Config.Models[math.random(1, #Config.Models)]
+    if Config.UseCustumPedModel then model = Config.CustumPedModel end
     loadModel(model)
     local ped = CreatePed(4, model, coords.x, coords.y, coords.z, 0, true, true)
     GiveWeaponToPed(ped, Config.Weapons[math.random(1, #Config.Weapons)], 999, false, true)
     SetPedIntoVehicle(ped, vehicle, seat)
+    SetPedOutfit(ped)
     SetEntityHealth(ped, 250)
     SetPedArmour(ped, 100)
     SetPedAsCop(ped, true)
