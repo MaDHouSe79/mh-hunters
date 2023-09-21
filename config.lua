807c2220093f0273929207a655c95afadf6b868b
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-21 17:30:50 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -26,3 +26,21 @@ Config.Bikes = {"sanchez", "sanchez2", "akuma", "carbonrs", "bagger", "daemon"}
 Config.Vehicles = {"vacca", "jester", "buffalo", "carbonizzare", "comet2", "dominator", "feltzer2", "fusilade"}
 Config.Helikopters = {"buzzard", "buzzard2", "annihilator", "maverick", "savage"}
 Config.Weapons = {"WEAPON_PISTOL", "WEAPON_PISTOL_MK2", "WEAPON_COMBATPISTOL", "WEAPON_APPISTOL", "WEAPON_STUNGUN"}
+
+Config.UseCustumPedModel = true
+Config.CustumPedModel = "mp_m_freemode_01"
+Config.Outfit = {
+    ['hair']    = {item = 19, texture = 1}, -- Hear
+    ['beard']   = {item = 2, texture = 0},  -- Beard
+    ["pants"]   = {item = 56, texture = 0}, -- Pants
+    ["arms"]    = {item = 30, texture = 0}, -- Arms
+    ["t-shirt"] = {item = 23, texture = 0}, -- T Shirt
+    ["vest"]    = {item = 18, texture = 0}, -- Body Vest
+    ["torso2"]  = {item = 74, texture = 3}, -- Jacket
+    ["shoes"]   = {item = 25, texture = 0}, -- Shoes
+    ["decals"]  = {item = 0, texture = 0},  -- Neck Accessory
+    ["bag"]     = {item = 0, texture = 0},  -- Bag
+    ["hat"]     = {item = 0, texture = 0},  -- Hat
+    ["glass"]   = {item = 0, texture = 0},  -- Glasses
+    ["mask"]    = {item = 0, texture = 0}   -- Mask
+}
