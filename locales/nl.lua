b1a022af725269384451894389659fe0bb378c89
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-07 19:20:00 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,7 +5,7 @@ local Translations = {
         ['hunter_alive'] = "~r~%{count}~s~ hunter zoekt je nog.",
         ['can_not_call_hunters'] = "Kan de hunters niet bellen, bel de politie!",
         ["you_lose_the_hunters"] = "De hunters zijn je uit het oog verloren!",
-        ["you_lose_the_hunters"] = "Een hunter is je uit het oog verloren!",
+        ["you_lose_the_hunter"] = "Een hunter is je uit het oog verloren!",
     },
 }
 
