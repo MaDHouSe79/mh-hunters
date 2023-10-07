48c5a6fb4c029cccae355d5ab931bb1686040d3a
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-07 19:20:45 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -5,7 +5,7 @@ local Translations = {
         ['hunter_alive'] = "~r~%{count}~s~ hunter is still looking for you.",
         ['can_not_call_hunters'] = "Can't call the hunters, call the police!",
         ["you_lose_the_hunters"] = "The hunters have lost track of you!",
-        ["you_lose_a_hunter"] = "A hunter have lost track of you!",
+        ["you_lose_the_hunter"] = "A hunter have lost track of you!",
     },
 }
 
