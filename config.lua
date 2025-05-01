b6d704c8a77a680ed1d264ecb1ecd89c2f5e6418
MaDHouSe <madhouse1979@gmail.com>
2025-05-01 11:47:14 +0200

    .

--- config.lua
+++ config.lua
@@ -32,56 +32,17 @@ Config.AmbulanceExport = exports['qb-ambulancejob']
 Config.UseCustumPedModel = true
 Config.CustumPedModel = "mp_m_freemode_01"
 Config.Outfit = {
-    ['hair'] = {
-        item = 19,
-        texture = 4
-    }, -- Hear
-    ['beard'] = {
-        item = 2,
-        texture = 0
-    }, -- Beard
-    ["pants"] = {
-        item = 10,
-        texture = 0
-    }, -- Pants
-    ["arms"] = {
-        item = 12,
-        texture = 0
-    }, -- Arms
-    ["t-shirt"] = {
-        item = 21,
-        texture = 0
-    }, -- T Shirt
-    ["vest"] = {
-        item = 0,
-        texture = 0
-    }, -- Body Vest
-    ["torso2"] = {
-        item = 32,
-        texture = 0
-    }, -- Jacket
-    ["shoes"] = {
-        item = 10,
-        texture = 0
-    }, -- Shoes
-    ["decals"] = {
-        item = 0,
-        texture = 0
-    }, -- Neck Accessory
-    ["bag"] = {
-        item = 0,
-        texture = 0
-    }, -- Bag
-    ["hat"] = {
-        item = 0,
-        texture = 0
-    }, -- Hat
-    ["glass"] = {
-        item = 23,
-        texture = 11
-    }, -- Glasses
-    ["mask"] = {
-        item = 0,
-        texture = 0
-    } -- Mask
+    ['hair'] = { item = 19, texture = 4 }, -- Hear
+    ['beard'] = { item = 2, texture = 0 }, -- Beard
+    ["pants"] = { item = 10, texture = 0 }, -- Pants
+    ["arms"] = { item = 12, texture = 0 }, -- Arms
+    ["t-shirt"] = { item = 21, texture = 0 }, -- T Shirt
+    ["vest"] = { item = 0, texture = 0 }, -- Body Vest
+    ["torso2"] = { item = 32, texture = 0 }, -- Jacket
+    ["shoes"] = { item = 10, texture = 0 }, -- Shoes
+    ["decals"] = { item = 0, texture = 0 }, -- Neck Accessory
+    ["bag"] = { item = 0, texture = 0 }, -- Bag
+    ["hat"] = {item = 0, texture = 0 }, -- Hat
+    ["glass"] = { item = 23, texture = 11}, -- Glasses
+    ["mask"] = { item = 0, texture = 0 } -- Mask
 }
